9b75f31e8c113f49d84388bba69e8c6d3c38cb79
Bogdan-Taran <taranbogdan9.2@mail.ru>
2024-07-11 21:06:13 +0700

    One Button - room bogdan

--- app/src/main/java/com/example/remotewifi/ActivityMyRoomBogdan.java
+++ app/src/main/java/com/example/remotewifi/ActivityMyRoomBogdan.java
@@ -3,23 +3,21 @@ package com.example.remotewifi;
 
 import static com.example.remotewifi.R.id.btn_podsvetka_stol;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.ConsumerIrManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.example.remotewifi.databinding.ActivityMainBinding;
-import com.example.remotewifi.databinding.ActivityMyRoomBogdanBinding;
-import com.example.remotewifi.databinding.ActivityTheHomeBinding;
 
 import java.io.IOException;
 
@@ -29,11 +27,15 @@ import okhttp3.Response;
 
 
 public class ActivityMyRoomBogdan extends AppCompatActivity {
+    ConsumerIrManager IrMan;
+    private static final int Rem_freq = 38000;
+    private static final int[] Rem_ON = {4600,4350,700,1550,650,1550,650,1600,650,450,650,450,650,450,650,450,700,400,700,1550,650,1550,650,1600,650,450,650,450,650,450,700,450,650,450,650,450,650,1550,700,450,650,450,650,450,650,450,650,450,700,400,650,1600,650,450,650,1550,650,1600,650,1550,650,1550,700,1550,650,1550,650};
     private Request request;    // добавляем переменную для запросов (OkHttp3)
     Button podsvetka_stol;
     Button svetilnik_stol;
     SharedPreferences pref;
     TextView podsvetkaStolTextView;
+    Button btnOnOffRGBLenta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +48,26 @@ public class ActivityMyRoomBogdan extends AppCompatActivity {
             return insets;
         });
 
+        IrMan = (ConsumerIrManager) this.getSystemService(Context.CONSUMER_IR_SERVICE);
+        btnOnOffRGBLenta = (Button) findViewById(R.id. btn_rgb_lenta);
+
+        btnOnOffRGBLenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!IrMan.hasIrEmitter()){
+                    Toast.makeText(getApplicationContext(), "No IR Emitter Found!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                IrMan.transmit(Rem_freq, Rem_ON);
+                Toast.makeText(getApplicationContext(), "IR Signal Sent!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     podsvetka_stol = (Button) findViewById(btn_podsvetka_stol);
     svetilnik_stol = (Button) findViewById(R.id.btn_svetilnik_stol);
     podsvetkaStolTextView = (TextView) findViewById(R. id. podsetka_stol);
+
 
 
         TextView trans_rgb_lenta = findViewById(R.id.rgb_lenta);
@@ -69,6 +88,8 @@ public class ActivityMyRoomBogdan extends AppCompatActivity {
         });
 
     }
+
+
 
     private void post(String post) {
         new Thread(new Runnable() { // новый второстепенный поток
